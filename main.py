0c58672f9da9afea235288dfb76a0a19849e112c
LightSaber42 <github.2019@mail.schieke.net>
2024-07-31 21:43:23 +0100

    first commit

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ def more_cards(request):
             hx_target="this"
         )
 
-if __name__ == '__main__': uvicorn.run("main:app", host='127.0.0.1', port=8001, reload=True)
+if __name__ == '__main__': uvicorn.run("main:app", host='0.0.0.0', port=8001, reload=True)
